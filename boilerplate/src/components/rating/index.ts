dc4859c17c452f94403451e29c248c496ada5964
Henrik Herbst Grubbe <henrikhgrubbe@gmail.com>
2025-04-09 19:24:03 +0200

    Add simple styling and support for mouse hovering

--- boilerplate/src/components/rating/index.ts
+++ boilerplate/src/components/rating/index.ts
@@ -1,27 +1,56 @@
-import { html, LitElement } from "lit";
-import { customElement, property } from "lit/decorators.js";
-import { starFilled, starUnfilled } from "./icons";
+import { css, html, LitElement } from "lit";
+import { customElement, property, state } from "lit/decorators.js";
+import { starBold } from "./icons";
 
 @customElement(`case-rating`)
 export class CaseRating extends LitElement {
+  static styles = css`
+    div {
+      display: flex;
+    }
+
+    span {
+      height: 24px;
+      width: 24px;
+      padding: 4px;
+      cursor: pointer;
+    }
+
+    svg {
+      fill: #d5d5d5;
+    }
+
+    span[filled] svg {
+      fill: #e9a93c;
+    }
+  `;
+
   @property({ type: Number }) public rating = 0;
+
+  @state() private _hoveredRating: number | null = null;
 
   private readonly range: number[] = [...Array(5).keys()].map((i) => i + 1);
 
-  protected render = () =>
-    html` <div>
+  protected render() {
+    const shownRating = this._hoveredRating ?? this.rating;
+
+    return html` <div>
       ${this.range.map(
         (i) =>
-          html`<span @click=${() => this.setRating(i)}
-            >${this.rating >= i
-              ? html`${starFilled}`
-              : html`${starUnfilled}`}</span
+          html`<span
+            ?filled=${shownRating >= i}
+            @mouseenter=${() => (this._hoveredRating = i)}
+            @mouseleave=${() => (this._hoveredRating = null)}
+            @click=${() => this.setRating(i)}
+            >${starBold}</span
           >`,
       )}
     </div>`;
+  }
 
-  private setRating(rating: number): void {
+  private setRating(rating: number) {
     this.rating = rating;
+    this._hoveredRating = null;
   }
 }
 
