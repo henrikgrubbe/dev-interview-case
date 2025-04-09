a54fa0703fe8a47fcfa3c157f7331d8cd95e7b34
Henrik Herbst Grubbe <henrikhgrubbe@gmail.com>
2025-04-09 10:13:41 +0200

    Functional example

--- boilerplate/src/components/rating/index.ts
+++ boilerplate/src/components/rating/index.ts
@@ -1,10 +1,27 @@
 import { html, LitElement } from "lit";
-import { customElement } from "lit/decorators.js";
+import { customElement, property } from "lit/decorators.js";
+import { starFilled, starUnfilled } from "./icons";
 
 @customElement(`case-rating`)
 export class CaseRating extends LitElement {
-  render() {
-    return html`<div>CaseRating component</div>`;
+  @property({ type: Number }) public rating = 0;
+
+  private readonly range: number[] = [...Array(5).keys()].map((i) => i + 1);
+
+  protected render = () =>
+    html` <div>
+      ${this.range.map(
+        (i) =>
+          html`<span @click=${() => this.setRating(i)}
+            >${this.rating >= i
+              ? html`${starFilled}`
+              : html`${starUnfilled}`}</span
+          >`,
+      )}
+    </div>`;
+
+  private setRating(rating: number): void {
+    this.rating = rating;
   }
 }
 
